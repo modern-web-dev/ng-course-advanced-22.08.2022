5025af2ca54981676b8f1a91a8dcb67fe193d6f4
Maciej Malecki <maciej.malecki@capgemini.com>
2022-08-22 13:17:07 +0200

    06 tests fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Book store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
+    expect(compiled.querySelector('article h1')?.textContent).toContain('Book store');
   });
 });
--- src/app/books/services/book.service.spec.ts
+++ src/app/books/services/book.service.spec.ts
@@ -1,16 +1,23 @@
-import { TestBed } from '@angular/core/testing';
 
 import { BookService } from './book.service';
+import {TestBed} from "@angular/core/testing";
 
 describe('BookService', () => {
   let service: BookService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [BookService]
+    });
     service = TestBed.inject(BookService);
+    // service = new BookService();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have three books', () => {
+    expect(service.getBooks()).toHaveSize(3);
   });
 });
